dbf76d6c9e8f1901dbec6d4cb5e6c629babf5b54
Ahsanul Haque Shakil <119407692+Ahsanul-Haque-Shakil@users.noreply.github.com>
2024-11-02 00:06:11 +0600

    Add files via upload

---
+++ about.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>about</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+   <!-- swiper css link  -->
+   <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<div class="container">
+
+<?php @include 'header.php'; ?>
+
+<section class="about">
+
+   <img src="pictures/about1.jpg" alt="">
+   <h3>about us</h3>
+   <p>Like in many countries around the world, weddings in Bangladesh are celebrated with lots of pomp, color, and zest. You need to learn the practices if you want to go and have your wedding there. Bangladeshi weddings are either Hindu or Muslim, but both share so many similarities. We have cut out the work for you so you can simply follow this guide.</p>
+   <a href="contact.php" class="btn">contact us</a>
+
+</section>
+
+<section class="team">
+
+   <h1 class="heading">our team</h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <img src="pictures/about2.jpg" alt="">
+         <h3>Afsana Orin</h3>
+         <p>wedding planner</p>
+         <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+            <a href="#" class="fab fa-twitter"></a>
+            <a href="#" class="fab fa-linkedin"></a>
+            <a href="#" class="fab fa-instagram"></a>
+         </div>
+      </div>
+
+      <div class="box">
+         <img src="pictures/about3.jpg" alt="">
+         <h3>Arpita Rahman</h3>
+         <p>wedding planner</p>
+         <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+            <a href="#" class="fab fa-twitter"></a>
+            <a href="#" class="fab fa-linkedin"></a>
+            <a href="#" class="fab fa-instagram"></a>
+         </div>
+      </div>
+
+      <div class="box">
+         <img src="pictures/about4.jpg" alt="">
+         <h3>Efti Mahbub</h3>
+         <p>wedding planner</p>
+         <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+            <a href="#" class="fab fa-twitter"></a>
+            <a href="#" class="fab fa-linkedin"></a>
+            <a href="#" class="fab fa-instagram"></a>
+         </div>
+      </div>
+
+      <div class="box">
+         <img src="pictures/about5.jpg" alt="">
+         <h3>Rafi Haque</h3>
+         <p>wedding planner</p>
+         <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+            <a href="#" class="fab fa-twitter"></a>
+            <a href="#" class="fab fa-linkedin"></a>
+            <a href="#" class="fab fa-instagram"></a>
+         </div>
+      </div>
+
+   </div>
+
+</section>
+
+<?php @include 'footer.php'; ?>
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- swiper js link  -->
+<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
